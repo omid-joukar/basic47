f0de2c6e4af850311ae104b466904cf043398038
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 16:06:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args){
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
